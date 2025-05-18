c169e09931c5ecf351c55566c79774803efe37ae
DimaM83 <150599187+DimaM83@users.noreply.github.com>
2025-05-19 00:05:38 +0200

    add votes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,65 @@
+// import styles from './App.module.css';
+// import CafeInfo from '../CafeInfo/CafeInfo';
+// import { Votes, VoteType } from '../../types/votes';
+// import { useState } from 'react';
+
+
+// export default function App() {
+//     const [votes, setVotes] = useState<Votes>({
+//         good: number;
+//         neutral: number;
+//         bad: number;
+//     });
+
+//     const handleVote = (type: VoteTupe) => {
+//         setVotes(prev => ({
+//             ...prev,
+//             [type]: prev[type] + 1,
+//         }));
+//     };
+
+//     const resetVotes = () => {
+//         setVotes({
+//             good: number;
+//             neutral: number;
+//             bad: number;
+//         });
+//     };
+//     return <div className={styles.app}>
+//         <CafeInfo />
+//     </div>;
+// }
+
+
+
+
+
+import React, { useState } from 'react';
 import styles from './App.module.css';
 import CafeInfo from '../CafeInfo/CafeInfo';
+import type { Votes, VoteType } from '../../types/votes';
 
 export default function App() {
-    return <div className={styles.app}>
-        <CafeInfo/>
-    </div>;
+    const [votes, setVotes] = useState<Votes>({
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    });
+
+    const handleVote = (type: VoteType) => {
+        setVotes(prev => ({
+            ...prev,
+            [type]: prev[type] + 1,
+        }));
+    };
+
+    const resetVotes = () => {
+        setVotes({ good: 0, neutral: 0, bad: 0 });
+    };
+
+    return (
+        <div className={styles.app}>
+            <CafeInfo />
+        </div>
+    );
 }
